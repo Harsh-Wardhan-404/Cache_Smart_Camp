6aad572b4dcd2ea4034d8b4549677244779012d0
AryaK19 <aryakadam348@gmail.com>
2024-09-27 11:50:35 +0530

    added fastapi

---
+++ fastapi/main.py
@@ -0,0 +1,19 @@
+
+from fastapi import FastAPI
+import uvicorn
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, port=port)
